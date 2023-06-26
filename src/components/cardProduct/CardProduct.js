e2c1d242a04ff5eaf3d5b2c006e2798311cca42d
Mạnh Quân <88712021+Mannj-nef@users.noreply.github.com>
2023-06-26 11:52:13 +0700

    remove console.log

--- src/components/cardProduct/CardProduct.js
+++ src/components/cardProduct/CardProduct.js
@@ -7,8 +7,6 @@ import "./style.scss";
 const CardProduct = ({ cardProduct }) => {
   const { isLoading } = useSelector((state) => state.productReducer);
 
-  console.log(isLoading);
-
   return (
     <div className="card-product-wrapper">
       {!isLoading &&
